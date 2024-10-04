e9a528b917f3eb0000dbaa6fa3088653e2c0caec
Vitor Guara <vrosaguara@my.bcit.ca>
2024-10-04 14:49:09 -0700

    added test.py to repo

---
+++ test.py
@@ -0,0 +1,59 @@
+from Dataset import Dataset
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.model_selection import GridSearchCV
+
+
+ds = Dataset('creditcard_2023.csv')
+
+# Seed defines a specific value for the randomness so to say
+# Basically if we use the same see, the same training and test sets will be created
+# This is useful to reproduce the same results
+# If we want a truly random data just run this without a seed (default is None)
+ds.create_train_test(ratio=0.7, seed=42)
+
+# 'Class' is how the fraud/legit binary is labeled in the dataset.
+ds.define_label_features(label='Class')
+
+ratios = [0.02, 0.05, 0.1, 0.15]
+
+ds.create_subsets(ratios=ratios)
+
+# for ratio, subset in ds.subsets.items():
+#     print(f"length of subset of ratio {ratio}: {len(subset)}")
+#     fraud_cases = subset[subset['Class'] == 1]
+#     total_cases = subset.shape[0]
+#     print(f"number of fraud cases: {fraud_cases.shape[0]}")
+#     print(f"number of total cases: {total_cases}")
+
+# model = SVC(kernel='rbf', C=10, gamma=0.00000001)
+model = SVC()
+param_grid = {
+    'C': [0.1, 1, 10, 100],
+}
+ds.reduce_test_data_ratio(0.05, 42)
+
+for ratio, subset in ds.subsets.items():
+    train_label = subset.loc[:,'Class']
+    train_features = subset.drop('Class', axis=1, inplace=False)
+    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=5, scoring='recall')
+    grid_search.fit(train_features, train_label)
+    # model.fit(train_features, train_label)  # Train the model using training features and labels
+    # test_predictions = model.predict(ds.test_features)
+    best_params = grid_search.best_params_
+    print(f"Best C value: {best_params['C']}")
+    best_model = grid_search.best_estimator_
+    test_predictions = best_model.predict(ds.test_features)
+
+    # Asked chatGPT for ways to best view the results
+    accuracy = accuracy_score(ds.test_label, test_predictions)
+    conf_matrix = confusion_matrix(ds.test_label, test_predictions)
+    report = classification_report(ds.test_label, test_predictions)
+
+    # Print the results
+    print(f"Support Vector Machine Model Performance for ratio {ratio} :")
+    print(f"Accuracy: {accuracy:.4f}")
+    print("Confusion Matrix:")
+    print(conf_matrix)
+    print("Classification Report:")
+    print(report)
